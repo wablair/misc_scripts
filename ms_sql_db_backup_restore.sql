cb2124df24b3ad599bab5a15ba592313ce4663a7
wablair <wablair@gmail.com>
2015-04-28 07:46:42 -0600

    Update ms_sql_db_backup_restore.sql

--- ms_sql_db_backup_restore.sql
+++ ms_sql_db_backup_restore.sql
@@ -1,1 +1,1 @@
-restore database DB_Name from disk = 'c:\DB_Name.bak' with move 'DB_Name' to 'E:\DB_Name\DB_Name.mdf', move 'EPCWID_log' to 'E:\DB_Name\DB_Name_log.ldf';
+restore database DB_Name from disk = 'c:\DB_Name.bak' with move 'DB_Name' to 'c:\DB_Name\DB_Name.mdf', move 'DB_Name_log' to 'c:\DB_Name\DB_Name_log.ldf';
