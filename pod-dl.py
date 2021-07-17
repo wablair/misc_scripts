0c38b68d3a6fdbe21b699a3d10e98effe935dd38
wablair <wablair@gmail.com>
2021-07-17 16:18:19 -0500

    Work on reverse download. Add prefix and output dir option.

--- pod-dl.py
+++ pod-dl.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3.8
 # pod-dl.py
 # Pass xml url and download all podcast episodes to current directory.
 # Doesn't handle errors well or label non-mp3 files correctly.
-# TODO: Stack urls and download oldest first maybe. Create XML file for adding
-# local copy to podcast apps.
+# TODO: Create XML file for adding local copy to podcast apps.
+#       Check output_dir before writting.
 
 """
-Copyright (c) 2018, William Blair <wablair@gmail.com>
+Copyright (c) 2018-2021, William Blair <wablair@gmail.com>
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
@@ -32,16 +32,24 @@ import io
 parser = argparse.ArgumentParser(description='Download podcasts from rss ' +
   'feed.')
 parser.add_argument('url')
+parser.add_argument('-p', '--prefix')
+parser.add_argument('-o', '--output_dir')
 args = parser.parse_args()
 feed_url = args.url
+output_dir = None
+prefix = None
+if args.output_dir:
+    output_dir = args.output_dir
+if args.prefix:
+    prefix = args.prefix
 
 # slugify() from django.utils.text
 """
 Copyright (c) Django Software Foundation and individual contributors.
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without modification
+, are permitted provided that the following conditions are met:
 
     1. Redistributions of source code must retain the above copyright notice,
        this list of conditions and the following disclaimer.
@@ -84,6 +92,7 @@ r = requests.get(feed_url)
 xml_file_like = io.BytesIO(r.content)
 tree = ET.parse(xml_file_like)
 root = tree.getroot()
+files = []
 for child in root:
     for child2 in child:
         if child2.tag == 'item':
@@ -91,19 +100,34 @@ for child in root:
             url = None
             for child3 in child2:
                 if child3.tag == 'title':
-                    filename = slugify(child3.text) + ".mp3"
+                    filename = "{}.mp3".format(slugify(child3.text))
                 if child3.tag == 'enclosure':
                     url = child3.attrib['url']
 
-                if (filename and url):
-                    if os.path.isfile(filename):
-                        continue
-                    print("Getting " + filename + " from " + url)
-                    try:
-                        r = requests.get(url, stream=True)
-                        r.raise_for_status()
-                        with open(filename, 'wb') as handle:
-                            for block in r.iter_content(1024):
-                                handle.write(block)
-                    except:
-                        continue
+                if filename and url and [url, filename] not in files:
+                    files.append([url, filename])
+
+count = 1
+
+files.reverse()
+
+for file in files:
+    url = file[0]
+    filename = file[1]
+    if os.path.isfile(filename):
+        continue
+    if prefix:
+        filename = "{}{:03d}_{}".format(prefix, count, filename)
+    if output_dir:
+        filename = "{}{}".format(output_dir, filename)
+    print("Getting {} from {}".format(filename, url))
+    try:
+        r = requests.get(url, stream=True)
+        r.raise_for_status()
+        with open(filename, 'wb') as handle:
+            for block in r.iter_content(1024):
+                handle.write(block)
+        count = count + 1
+    except:
+        print("Could not get {}".format(url))
+        continue
